01610a7aa78da1dad54a171f7b5e8cec454b6e47
Oyetoro Stephen <toro90@ymail.com>
2017-12-20 11:22:08 +0100

    My First Website

---
+++ contactform.php
@@ -0,0 +1,10 @@
+<?php 
+$to = 'oyetorostepven@gmail.com';
+$subject = 'Message from Contact Demo ';
+$body = filter_var($_POST['Message'], FILTER_SANITIZE_STRING);
+if (mail ($to, $subject, $body)) {
+echo "sent";
+} else {
+echo "fail";
+}
+?>
